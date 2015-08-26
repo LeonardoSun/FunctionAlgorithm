5016664c40eea76e1295bfbe024ddab241eed4d9
LeonardoSun <54fengyi@sohu.com>
2015-08-26 20:02:37 +0800

    init

---
+++ UnitTestProject1/UnitTestUnknownNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuncAlg;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestUnknownNumber
+    {
+        [TestMethod]
+        public void TestMultiplyConst()
+        {
+            UnknownNumber x = new UnknownNumber("x");
+            var r = x * 5;
+            Console.WriteLine("name:{0}", r.name);
+
+            UnknownNumber y = new UnknownNumber(5, "y");
+            r = y * 7;
+            Console.WriteLine("name:{0}", r.name);
+        }
+
+        [TestMethod]
+        public void TestDifferentVarMultiply()
+        {
+            UnknownNumber u1 = new UnknownNumber("x");
+            UnknownNumber y1 = new UnknownNumber(3, "y");
+            var r = u1 * y1;
+            Console.WriteLine("name:{0}", r.name);
+        }
+    }
+}
